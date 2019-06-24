2220e2cab243e3fbd7afc62c9f40fd5929a29870
Artur Krysiak <artur.krysiak.warszawa@gmail.com>
2019-06-24 17:51:36 +0200

    Working serial monitoring

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,89 @@
+#include <FS.h>                   //this needs to be first, or it all crashes and burns...
+
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+
+//needed for library
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
+
 #include <Arduino.h>
 
-static const uint8_t ARDUINO_NODECMU_V3_BUILT_IN_LED   = D4;
+#include <SoftwareSerial.h>
 
-static const boolean WORKING_PROGRAM   = true;
 
-void setup()
-{
-    // initialize LED digital pin as an output.
-    if (WORKING_PROGRAM) {
-        pinMode(ARDUINO_NODECMU_V3_BUILT_IN_LED, OUTPUT);
+SoftwareSerial ESPserial(2, 3); // RX | TX
+
+static const uint8_t ARDUINO_NODECMU_V3_BUILT_IN_LED = D4;
+
+static const boolean WORKING_PROGRAM = true;
+
+//gets called when WiFiManager enters configuration mode
+void configModeCallback(WiFiManager *myWiFiManager) {
+    Serial.println("Entered config mode");
+    Serial.println(WiFi.softAPIP());
+    //if you used auto generated SSID, print it
+    Serial.println(myWiFiManager->getConfigPortalSSID());
+}
+
+void setup() {
+    if (!WORKING_PROGRAM) {
+        return;
+    }
+
+    //   ***  Additional Code that fixed my problem  ****
+    Serial.begin(9600);
+    ESPserial.begin(9600);
+    delay(1000);
+
+    Serial.println("Ready");
+    ESPserial.println("AT+GMR");
+
+// ***   End of additional code
+    unsigned long startedAt = millis();
+
+    WiFi.printDiag(Serial); //Remove this line if you do not want to see WiFi password printed
+    Serial.println("Opening configuration portal");
+    //Local initialization. Once its business is done, there is no need to keep it around
+
+    WiFiManager wifiManager;
+    //sets timeout in seconds until configuration portal gets turned off.
+    //If not specified device will remain in configuration mode until
+    //switched off via webserver.
+    if (WiFi.SSID() != "")
+        wifiManager.setConfigPortalTimeout(60); //If no access point name has been previously entered disable timeout.
+
+    //it starts an access point
+    //and goes into a blocking loop awaiting configuration
+    //Delete these two parameters if you do not want a WiFi password on your configuration access point
+    if (wifiManager.startConfigPortal("webhook-button",
+                                      "72ad2cc8-5756-4804-b4d8-ae5cbacfab31")) {
+        //if you get here you have connected to the WiFi
+        Serial.println("connected...yeey :)");
+    } else {
+        Serial.println("Not connected to WiFi but continuing anyway.");
+    }
+    // For some unknown reason webserver can only be started once per boot up
+    // so webserver can not be used again in the sketch.
+
+    Serial.print("After waiting ");
+    int connRes = WiFi.waitForConnectResult();
+    float waited = (millis() - startedAt);
+    Serial.print(waited / 1000);
+    Serial.print(" secs in setup() connection result is ");
+    Serial.println(connRes);
+    if (WiFi.status() != WL_CONNECTED) {
+        Serial.println("failed to connect, finishing setup anyway");
+    } else {
+        Serial.print("local ip: ");
+        Serial.println(WiFi.localIP());
     }
 }
 
-void loop()
-{
-    if (WORKING_PROGRAM) {
-        // turn the LED off (HIGH is the voltage level)
-        digitalWrite(ARDUINO_NODECMU_V3_BUILT_IN_LED, HIGH);
-        Serial.println("Turned off");
-        // wait for a second
-        delay(1000);
-        // turn the LED on by making the voltage LOW
-        digitalWrite(ARDUINO_NODECMU_V3_BUILT_IN_LED, LOW);
-        Serial.println("Turned on");
-        // wait for a second
-        delay(1000);
+void loop() {
+    if (!WORKING_PROGRAM) {
+        return;
     }
+
+    Serial.println("Working?");
 }
